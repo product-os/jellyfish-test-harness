d0354deb0b8c6de50e65d4c191afaaea539b5525
Josh Bowling <josh@balena.io>
2022-01-03 13:48:09 +0900

    Bump jellyfish-queue and jellyfish-core
    Change-type: patch
    Signed-off-by: Josh Bowling <josh@balena.io>

--- lib/integration/backend-helpers.ts
+++ lib/integration/backend-helpers.ts
@@ -1,6 +1,5 @@
 // TODO: Remove in favor of helpers.ts
-import * as core from '@balena/jellyfish-core';
-import { MemoryCache } from '@balena/jellyfish-core';
+import { Cache, Kernel } from '@balena/jellyfish-core';
 import { defaultEnvironment } from '@balena/jellyfish-environment';
 import { v4 as uuidv4 } from 'uuid';
 import type { BackendTestContext, SetupOptions } from '../types';
@@ -18,7 +17,7 @@ export async function before(
 	const suffix = options.suffix ? options.suffix : uuidv4();
 	const dbName = `test_${suffix.replace(/-/g, '_')}`;
 
-	const cache = new MemoryCache(
+	const cache = new Cache(
 		Object.assign({}, defaultEnvironment.redis, {
 			namespace: dbName,
 		} as any),
@@ -29,7 +28,7 @@ export async function before(
 		id: `CORE-TEST-${uuidv4()}`,
 	};
 
-	const kernel = await core.create(
+	const kernel = await Kernel.withPostgres(
 		logContext,
 		cache,
 		Object.assign({}, defaultEnvironment.database.options, {
